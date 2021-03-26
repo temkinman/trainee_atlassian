fadc87bce63e83c027b7ddef561ee1000546ad09
temkinman <temkinman@mail.ru>
2021-03-26 13:40:31 +0300

    tryiun using cucumber

--- src/Components/LeftSide.js
+++ src/Components/LeftSide.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import AppFrame from './common/app-frame'; //where can I take it?
+import style from './module.style.css'
+// import AppFrame from './common/app-frame'; //where can I take it?
 import {
   SideNavigation,
   Section,
@@ -12,13 +13,42 @@ import {
   NavigationFooter,
 } from "@atlaskit/side-navigation";
 
+
+const ins = `
+@alltagged
+Feature: My Feature
+
+  A big feature file
+
+  # is a comment
+  @tag1
+  Scenario: Automates stuff
+    Given a bunch of old stuff
+    # more comments here
+    When i try to compile
+    Then I get results
+`;
+
+const Gherkin = require('@cucumber/gherkin')
+var parser = new Gherkin.Parser();
+console.log('parser', parser);
+var gherkinDocument = parser.parse(ins);
+console.log('gherkinDocument', gherkinDocument);
+var pickles = new Gherkin.Compiler().compile(gherkinDocument);
+
+
 const LeftSide = () => {
   return (
-    <AppFrame hideAppBar hideBorder>
-      <SideNavigation label="project">
-        <span />
+    // <AppFrame hideAppBar hideBorder>
+      <SideNavigation label="project" id="leftSide">
+        <p>Test panel</p>
+        <p>Test panel</p>
+        <p>Test panel</p>
+        <p>Test panel</p>
+        <p>Test panel</p>
       </SideNavigation>
-    </AppFrame>
+
+    // </AppFrame>
   );
 };
 
